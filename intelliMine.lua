69927520c47e39f89c3b9bb6d1ca62962f151fd6
Pierre Kisters <pierrekisters@gmail.com>
2021-02-18 18:39:33 +0100

    Update intelliMine.lua

--- intelliMine.lua
+++ intelliMine.lua
@@ -110,24 +110,41 @@ local function turtleForceGo(dig, attack)
     
     return false
   end
+  
+  local function forceGoBack(keepRotation)
+    if not refuel(1) then return false end
+    
+    if turtleRaw.go.back() then return true end
+    
+    turtleRaw.turn.right()
+    turtleRaw.turn.right()
+    local result = forceGo("forward")
+    if (keepRotation) then
+      turtleRaw.turn.left()
+      turtleRaw.turn.left()
+    end
+    return result
+  end
  
   return {
     forward = function() return forceGo("forward") end,
     up = function() return forceGo("up") end,
     down = function() return forceGo("down") end,
-    back = function()
-      if not refuel(1) then return false end
-      
-      if turtleRaw.go.back() then return true end
-      
-      turtleRaw.turn.right()
-      turtleRaw.turn.right()
-      local result = forceGo("forward")
-      turtleRaw.turn.left()
-      turtleRaw.turn.left()
-      return result
-    end,
+    back = function() return forceGoBack(true) end,
+    backAnyRotation = function() return forceGoBack(false) end,
   }
+end
+
+local function rotationOffsetBy(rotation, other)
+  if rotation == nil or other == nil then return nil end
+  
+  return (rotation + other + 4) % 4
+end
+
+local function rotationOffsetTo(rotation, other)
+  if rotation == nil or other == nil then return nil end
+  
+  return (other - rotation + 4) % 4
 end
 
 local function Vec(x, y, z)
@@ -146,14 +163,32 @@ local function Vec(x, y, z)
     copy = function(self)
       return Vec(self.x, self.y, self.z)
     end,
+    withX = function(self, x)
+      return Vec(x, self.y, self.z)
+    end,
+    withY = function(self, y)
+      return Vec(self.x, y, self.z)
+    end,
+    withZ = function(self, z)
+      return Vec(self.x, self.y, z)
+    end,
     isNull = function(self)
-      return self.x == 0 and self.y == 0 and self.z == 0
+      return (not self.x or self.x == 0) and (not self.y or self.y == 0) and (not self.z or self.z == 0)
     end,
-    offset = function(self, other)
-      return Vec(self.x + other.x, self.y + other.y, self.z + other.z)
+    isAt = function(self, other)
+      if other == nil then return nil end
+      
+      return (not self.x or not other.x or self.x == other.x) and (not self.y or not other.y or self.y == other.y) and (not self.z or not other.z or self.z == other.z)
     end,
-    to = function(self, other)
-      return Vec(other.x - self.x, other.y - self.y, other.z - self.z)
+    offsetBy = function(self, other)
+      if other == nil then return nil end
+      
+      return Vec(self.x and other.x and (self.x + other.x), self.y and other.y and (self.y + other.y), self.z and other.z and (self.z + other.z))
+    end,
+    offsetTo = function(self, other)
+      if other == nil then return nil end
+      
+      return Vec(self.x and other.x and (other.x - self.x), self.y and other.y and (other.y - self.y), self.z and other.z and (other.z - self.z))
     end,
     xzRotation = function(self)
       if self.x == 0 and self.z < 0 then return 0 end
@@ -163,11 +198,21 @@ local function Vec(x, y, z)
       return nil
     end,
     offsetByRotation = function(self, rotation)
-      rotation = rotation % 4
+      rotation = rotationOffsetBy(rotation, 0)
       if rotation == 0 then return Vec(self.x, self.y, self.z - 1) end
       if rotation == 1 then return Vec(self.x + 1, self.y, self.z) end
       if rotation == 2 then return Vec(self.x, self.y, self.z + 1) end
       if rotation == 3 then return Vec(self.x - 1, self.y, self.z) end
+      return nil
+    end,
+    step = function(self)
+      if self.y and (not self.x or math.abs(self.x) <= math.abs(self.y)) and (not self.z or math.abs(self.z) <= math.abs(self.y)) then
+        return Vec(0, self.y / math.abs(self.y), 0)
+      elseif self.x and (not self.z or math.abs(self.z) <= math.abs(self.x)) then
+        return Vec(self.x / math.abs(self.x), 0, 0)
+      elseif self.z then
+        return Vec(0, 0, self.z / math.abs(self.z))
+      end
       return nil
     end,
     string = function(self)
@@ -176,9 +221,14 @@ local function Vec(x, y, z)
   }
 end
 
-local function moveRaw(x, y, z)
-  print("test")
-end
+local offset = {
+  up = function()
+    return Vec(0, 1, 0)
+  end,
+  down = function()
+    return Vec(0, -1, 0)
+  end,
+}
 
 local function locate()
   local position = Vec(gps.locate())
@@ -195,12 +245,11 @@ local function locate()
       go.back()
     elseif go.back() then
       offsetPosition = Vec(gps.locate())
-      rotationOffset = rotationOffset + 2
+      rotationOffset = rotationOffsetBy(rotationOffset, 2)
       go.forward()
     end
     
-    local rotation = position:to(offsetPosition):xzRotation()
-    return rotation and (rotation + rotationOffset) % 4
+    return rotationOffsetBy(position:offsetTo(offsetPosition):xzRotation(), rotationOffset)
   end
   
   local rotation = rotationByMove(turtleRaw.go)
@@ -225,24 +274,40 @@ end
 
 globalPosition, globalRotation = locate()
 
+offset.forward = function()
+  return Vec():offsetByRotation(globalRotation)
+end
+
+local function isAt(position)
+  return globalPosition:isAt(position)
+end
+
+local function offsetTo(position)
+  return globalPosition:offsetTo(position)
+end
+
+local function offsetBy(vector)
+  return globalPosition:offsetBy(vector)
+end
+
 local updateGlobalPositionAndRotation = {
   forward = function()
     globalPosition = globalPosition:offsetByRotation(globalRotation)
   end,
   up = function()
-    globalPosition = globalPosition:offset(Vec(0, 1, 0))
+    globalPosition = globalPosition:offsetBy(Vec(0, 1, 0))
   end,
   down = function()
-    globalPosition = globalPosition:offset(Vec(0, -1, 0))
+    globalPosition = globalPosition:offsetBy(Vec(0, -1, 0))
   end,
   back = function()
-    globalPosition = globalPosition:offsetByRotation((globalRotation + 2) % 4)
+    globalPosition = globalPosition:offsetByRotation(rotationOffsetBy(globalRotation, 2))
   end,
   left = function()
-    globalRotation = (globalRotation + 3) % 4
+    globalRotation = rotationOffsetBy(globalRotation, 3)
   end,
   right = function()
-    globalRotation = (globalRotation + 1) % 4
+    globalRotation = rotationOffsetBy(globalRotation, 1)
   end,
 }
 
@@ -284,8 +349,8 @@ turtleRaw = (function()
 end)()
 
 local function rotateBy(rotation)
-  rotation = rotation % 4
-  if (rotation == 3) then return turtleRaw.turn.left() end
+  rotation = rotationOffsetBy(rotation, 0)
+  if rotation == 3 then return turtleRaw.turn.left() end
   for i = 1, rotation do
     if not turtleRaw.turn.right() then return false end
   end
@@ -293,86 +358,50 @@ local function rotateBy(rotation)
 end
 
 local function rotateTo(rotation)
-  return rotateBy(rotation - globalRotation)
+  return rotateBy(rotationOffsetTo(rotation, globalRotation))
 end
 
---local function moveBy(vector) end
-
-
-
-
-function go(dig, attack, wait)
- if forceAction("go", "", dig, attack, wait) then
-  xAdd, zAdd = getRotationOffset(r)
-  addCoords(xAdd, 0, zAdd)
-  return true
- end
- return false
-end
-
-function turn(arg_r)
- diff = arg_r - r
- if math.abs(diff)==3 then
- diff = diff / -3
- end
- for i=1,math.abs(diff),1 do
-  if diff>0 then
-   turnRight()
-  else
-   turnLeft()
+local function moveStepBy(go, vector)
+  local step = vector:step()
+  local rotationOffset = rotationOffsetTo(step:xzRotation(), globalRotation)
+  
+  if (step:isNull()) then
+    return true
+  elseif rotationOffset == 2 then
+    return (go.backAnyRotation or go.back)()
+  elseif rotationOffset ~= nil then
+    rotateBy(rotationOffset)
+    return go.forward()
+  elseif step.y > 0 then
+    return go.up()
+  elseif step.y < 0 then
+    return go.down()
   end
- end
 end
 
-function turnRight(count)
- if count == nil then count = 1 end
- for i = 1, count, 1 do
-  turtle.turnRight()
-  addRotation(1)
- end
+function goStepXZ(xzPosition, dig, attack, wait, alternate)
+  local forceGo = turtleForceGo(dig, attack)
+  return moveStepBy(forceGo, xzPosition:withY(nil))
 end
 
-function turnLeft()
- if count == nil then count = 1 end
- for i = 1, count, 1 do
-  turtle.turnLeft()
-  addRotation(-1)
- end
+function goStepY(y, dig, attack, wait)
+  local forceGo = turtleForceGo(dig, attack)
+  return moveStepBy(forceGo, Vec(nil, y, nil))
 end
 
-function goStep(arg_x, arg_z, dig, attack, wait, alternate)
- if alternate == nil then alternate = false end
- if arg_x == 0 or arg_z == 0 then alternate = false end
- turn(getRotationFor(arg_x, arg_z, alternate))
- return go(dig, attack, wait)
-end
-
-function goStepY(arg_y, dig, attack, wait)
- if arg_y > 0 then
-  return up(dig, attack, wait)
- elseif arg_y < 0 then
-  return down(dig, attack, wait)
- end
- return true
-end
-
-function goTo(arg_x, arg_y, arg_z, arg_r, dig, attack, wait, straight)
- if arg_z == nil then
-  arg_z = arg_y
-  arg_y = y
- end
+function goTo(position, rotation, dig, attack, wait, straight)
  if dig == nil then dig = false end
  if attack == nil then attack = true end
  if wait == nil then wait = true end
  if straight == nil then straight = false end
- local oldY = y
- while not isAt(arg_x, arg_y, arg_z) do
-  if not isAt(arg_x, arg_z) then
+ local oldY = position.y
+ while not isAt(position) do
+  if not isAt(position:withY(nil)) then
    local alternate = 0
    local dodge = -1
    if straight then alternate = 2 end
-   if arg_y >= oldY then dodge = dodge * -1 end
-   while not goStep(arg_x - x, arg_z - z, dig and alternate == 2, attack, false) and not goStep(arg_x - x, arg_z - z, dig and alternate == 2, attack, false, true) do
+   if position.y >= oldY then dodge = dodge * -1 end
+   while not goStepXZ(offsetTo(position), dig and alternate == 2, attack, false) and not goStepXZ(offsetTo(position), dig and alternate == 2, attack, false, true) do
     if not goStepY(dodge, dig and alternate == 2, attack, false) then
 	 if alternate == 0 then
 	  alternate = alternate + 1
@@ -391,12 +420,12 @@ function goTo(arg_x, arg_y, arg_z, arg_r, dig, attack, wait, straight)
 	end
     sleep(0.2)
    end
-  elseif not isAt(arg_y) then
-   if not goStepY(arg_y - y, true, attack, false) and not wait then return false end
+  elseif not isAt(Vec(nil, position.y, nil)) then
+   if not goStepY(position.y - globalPosition.y, true, attack, false) and not wait then return false end
   end
   sleep(0.2)
  end
- if arg_r ~= nil then turn(arg_r) end
+ if rotation ~= nil then rotateTo(rotation) end
  return true
 end
 
@@ -408,42 +437,41 @@ slot_ignore = 0
 minFuelLevel = 10000
 
 -- global vars
-digStack = {}
-
-local function sizeDigTask()
- local i = 1
- while digStack[i] ~= nil do
-  i = i + 1
- end
- return i - 1
+local function DigStack()
+  return {
+    size = function(self)
+      local i = 1
+      while self[i] ~= nil do i = i + 1 end
+      return i - 1
+    end,
+    push = function(self, position, rotation)
+      local i = 1
+      while self[i] ~= nil do
+        if self[i].x == position.x and self[i].y == position.y and self[i].z == position.z then
+          if self[i].r == nil then self[i].r = rotation end
+          return
+        end
+        i = i + 1
+      end
+      self[i] = {
+        x = position.x,
+        y = position.y,
+        z = position.z,
+        r = rotation
+      }
+    end,
+    pop = function(self)
+      local i = sizeDigTask()
+      local position, rotation = self[i] and Vec(self[i].x, self[i].y, self[i].z), self[i] and self[i].r
+      if self[i] then self[i] = nil end
+      return position, rotation
+    end,
+  }
 end
 
-local function pushDigTask(x, y, z, r)
- local i = 1
- while digStack[i] ~= nil do
-  if digStack[i][1]==x and digStack[i][2]==y and digStack[i][3]==z then
-   if digStack[i][4]==nil then digStack[i][4]=r end
-   return
-  end
-  i = i + 1
- end
- digStack[i] = {}
- digStack[i][1] = x
- digStack[i][2] = y
- digStack[i][3] = z
- digStack[i][4] = r
-end
-
-local function popDigTask()
- local i = sizeDigTask()
- if digStack[i] == nil then return end
- local x, y, z, r = digStack[i][1], digStack[i][2], digStack[i][3], digStack[i][4]
- digStack[i] = nil
- return x, y, z, r
-end
+digStack = DigStack()
 
 function init()
- setRefuelHandler(refuel)
  slot_ignore_max = slot_ignore
  for i = slot_ignore, 16, 1 do
   if turtle.getItemCount(i) > 0 then
@@ -478,35 +506,34 @@ function dumpIfNeeded()
  if shouldDump() then dumpItems() end
 end
 
-function isOre(dir)
- if dir==nil then dir="" end
- if not getAction("detect", dir)() then return false end
- for i = slot_ignore, slot_ignore_max, 1 do
-  turtle.select(i)
-  if getAction("compare", dir)() then
-   turtle.select(slot_items)
-   return false
+local isOre = (function()
+  local function isOre(dir)
+    if not turtleRaw.detect[dir]() then return false end
+    for i = slot_ignore, slot_ignore_max, 1 do
+      turtle.select(i)
+      if turtleRaw.compare[dir]() then
+        turtle.select(slot_items)
+        return false
+      end
+    end
+    turtle.select(slot_items)
+    return true
   end
- end
- turtle.select(slot_items)
- return true
-end
+  
+  return {
+    forward = function() return isOre("forward") end,
+    up = function() return isOre("up") end,
+    down = function() return isOre("down") end,
+  }
+end)()
 
-function checkAround(back)
- if back==nil then back = false end
- if isOre() then pushDigTask(getX() + getRotationOffsetX(), getY(), getZ() + getRotationOffsetZ()) end
- if isOre("up") then pushDigTask(getX(), getY() + 1, getZ()) end
- if isOre("down") then pushDigTask(getX(), getY() - 1, getZ()) end
- turnLeft()
- if isOre() then pushDigTask(getX() + getRotationOffsetX(), getY(), getZ() + getRotationOffsetZ()) end
- turnRight(2)
- if isOre() then pushDigTask(getX() + getRotationOffsetX(), getY(), getZ() + getRotationOffsetZ()) end
- if back then
-  turnRight()
-  if isOre() then pushDigTask(getX() + getRotationOffsetX(), getY(), getZ() + getRotationOffsetZ()) end
-  turnLeft()
+function checkAround()
+ for i = 1, 4 do
+   if isOre.forward() then digStack:push(offsetBy(offset.forward())) end
+   rotateBy(1)
  end
- turnLeft()
+ if isOre.up() then digStack:push(offsetBy(offset.up())) end
+ if isOre.down() then digStack:push(offsetBy(offset.down())) end
 end
 
 function dig(count, up)
@@ -524,9 +551,9 @@ function dig(count, up)
    dumpIfNeeded()
    turtle.select(slot_items)
    if up and x==nil then digUp() end
-   x, y, z, r = popDigTask()
+   position, r = digStack:pop()
    if x==nil then break end
-   check = goTo(x, y, z, r, true, true, false, true)
+   check = goTo(position.x, position.y, position.z, r, true, true, false, true)
   end
  end
  return ret
